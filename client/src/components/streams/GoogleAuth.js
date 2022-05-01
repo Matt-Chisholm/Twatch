ee0d2baa72d32e735e2166af67210e9c7cc8fa6b
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-05-01 12:55:59 -0700

    reducer for auth

--- client/src/components/streams/GoogleAuth.js
+++ client/src/components/streams/GoogleAuth.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../../actions';
 
-export default class GoogleAuth extends Component {
+class GoogleAuth extends Component {
     state = { isSignedIn: null };
 
     componentDidMount() {
@@ -8,7 +10,7 @@ export default class GoogleAuth extends Component {
             window.gapi.client.init({
                 clientId: "172316044644-hnphun5bhls9phfikjnjdg9vd4hk2jfv.apps.googleusercontent.com",
                 scope: 'email'
-            }).then (() => {
+            }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
                 this.setState({ isSignedIn: this.auth.isSignedIn.get() });
                 this.auth.isSignedIn.listen(this.onAuthChange);
@@ -16,8 +18,12 @@ export default class GoogleAuth extends Component {
         });
     };
 
-    onAuthChange = () => {
-        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    onAuthChange = (isSignedIn) => {
+        if (isSignedIn) {
+            this.props.signIn();
+        } else {
+            this.props.signOut();
+        }
     };
 
     onSignInClick = () => {
@@ -55,3 +61,5 @@ export default class GoogleAuth extends Component {
         )
     }
 }
+
+export default connect(null, { signIn, signOut })(GoogleAuth);
---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+    isSignedIn: null
+};
+
+export default (state = initialState, { type, payload }) => {
+    switch (type) {
+
+        case 'SIGN_IN':
+            return { ...state, isSignedIn: true }
+
+        case 'SIGN_OUT':
+            return { ...state, isSignedIn: false }
+
+        default:
+            return state
+    }
+}
