f369dd8d2ff96e41cc6cb83ce8d69f5ecabc4266
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-05-02 09:55:09 -0700

    new streams being created properly and RESTfully

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import reducers from "./reducers";
+import reduxThunk from 'redux-thunk';
 
-const store = createStore(reducers);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
