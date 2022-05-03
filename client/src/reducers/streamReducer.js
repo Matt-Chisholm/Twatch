c5dee2c03b5c849491b9a48e7908f0a7b9aa4b83
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-05-03 12:33:52 -0700

    starting REST switch

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -5,3 +5,21 @@ import {
     EDIT_STREAM,
     DELETE_STREAM
 } from '../actions/types';
+
+export default (state = {}, action) => {
+    switch (action.type) {
+        case FETCH_STREAM:
+            return { ...state, [action.payload.id]: action.payload };
+        case FETCH_STREAMS:
+            return { ...state, [action.payload.id]: action.payload };
+        case CREATE_STREAM:
+            return { ...state, [action.payload.id]: action.payload };
+        case EDIT_STREAM:
+            return { ...state, [action.payload.id]: action.payload };
+        case DELETE_STREAM:
+            return { ...state }
+
+        default:
+            return state;
+    }
+};
