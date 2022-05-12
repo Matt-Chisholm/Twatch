3441dd8b8e23ce26da75059dbfff5a7bf49783d2
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-05-12 09:51:43 -0700

    showing stream title

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamEdit = (props) => {
-  console.log(props);
 
-  return (
-    <div>StreamEdit</div>
-  )
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading..</div>
+    }
+    return <div>{this.props.stream.title}</div>
+  }
 };
 
 const mapStateToProps = (state, ownProps) => {
